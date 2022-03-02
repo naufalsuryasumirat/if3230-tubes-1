14f6d71c9c6defb445a810b893ea1516468c8787
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2022-03-02 15:15:23 +0700

    init mpi

--- src/parallel.c
+++ src/parallel.c
@@ -5,29 +5,50 @@
 #include <omp.h>
 #include <time.h>
 #include <stdio.h>
+#include <serial.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+// argv[1]: file path; argv[2]: 
+int main(int argc, char* argv[]) {
+	int kernel_row, kernel_col, target_row, target_col, num_targets;
+	int world_size, world_rank, num_threads;
+	
+	clock_t begin = clock();
+
+	file = fopen(argv[1], "r");
+	if (!file) {
+		printf("File read error\n");
+		return 0;
+	}
+
+	sscanf(argv[2], "%d", &num_threads /* banyaknya thread */);
+
+	fscanf(file, "%d %d", &kernel_row, &kernel_col);
+	Matrix kernel = input_matrix(kernel_row, kernel_col);
+
+	fscanf(file, "%d %d %d", &num_targets, &target_row, &target_col);
+	Matrix* arr_mat = (Matrix*)malloc(num_targets * sizeof(Matrix));
+	int arr_range[num_targets];
+
 	MPI_Init(NULL, NULL);
 
-	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-
-
-	#pragma omp parallel num_threads(8)
+	#pragma omp parallel num_threads(num_threads)
 	{
 		int nthreads, tid;
 		nthreads = omp_get_num_threads();
 		tid = omp_get_thread_num();
-		printf("Hello world from processor %s, rank %d out of %d processors, from thread %d out of %d threads\n", processor_name, world_rank, world_size, tid, nthreads);
+		printf("Hello world from rank %d out of %d processors, 
+			from thread %d out of %d threads, %s\n", 
+			world_rank, world_size, tid, nthreads);
 	}
 
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
 	MPI_Finalize();
+
+	return 0;
 }
